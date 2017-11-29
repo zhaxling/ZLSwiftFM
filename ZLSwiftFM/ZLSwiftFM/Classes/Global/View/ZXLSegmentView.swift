0b724c6e715d9d52888a31e75c3c014194419671
GitHubCha2016 <GitHub2016>
2017-11-29 11:31:05 +0800

    添加SegmentView

---
+++ ZLSwiftFM/ZLSwiftFM/Classes/Global/View/ZXLSegmentView.swift
@@ -0,0 +1,21 @@
+//
+//  ZXLSegmentView.swift
+//  ZLSwiftFM
+//
+//  Created by Apple on 2017/11/29.
+//  Copyright © 2017年 zl. All rights reserved.
+//
+
+import UIKit
+
+class ZXLSegmentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
